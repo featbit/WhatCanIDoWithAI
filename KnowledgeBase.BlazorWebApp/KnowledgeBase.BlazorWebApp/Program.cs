794f46840daa089e2c75e156203f84244c442469
Comiscience <hu-beau@outlook.com>
2025-02-20 00:17:31 +0800

    chore

--- KnowledgeBase.BlazorWebApp/KnowledgeBase.BlazorWebApp/Program.cs
+++ KnowledgeBase.BlazorWebApp/KnowledgeBase.BlazorWebApp/Program.cs
@@ -1,5 +1,6 @@
 using KnowledgeBase.BlazorWebApp.Client.Pages;
 using KnowledgeBase.BlazorWebApp.Components;
+using FeatBit.Sdk.Server.DependencyInjection; // Add this using directive
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,6 +8,15 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents()
     .AddInteractiveWebAssemblyComponents();
+
+// Add FeatBit .NET Server SDK
+builder.Services.AddFeatBit(options =>
+{
+    options.EnvSecret = "dnja-RQ0JEWY7DO7TwGc9gq6T7F-uqb0K6zBegwsPUHw";
+    options.StreamingUri = new Uri("wss://app-eval.featbit.co");
+    options.EventUri = new Uri("https://app-eval.featbit.co");
+    options.StartWaitTime = TimeSpan.FromSeconds(3);
+});
 
 var app = builder.Build();
 
@@ -23,7 +33,6 @@ else
 }
 
 app.UseHttpsRedirection();
-
 
 app.UseAntiforgery();
 
