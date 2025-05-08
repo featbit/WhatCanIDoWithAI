5c9b144863bcc35f2f2ccc5d253f2528e72e56b4
Comiscience <hu-beau@outlook.com>
2025-05-08 23:52:04 +0800

    chore

--- FeatGen.OpenAI/Extensions.cs
+++ FeatGen.OpenAI/Extensions.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Http.Resilience;
 
 namespace FeatGen.OpenAI
 {
@@ -10,7 +12,24 @@ namespace FeatGen.OpenAI
             builder.Services.AddTransient<IOpenAiChatService, OpenAiChatService>();
             builder.Services.AddScoped<IAntropicChatService, AntropicChatService>();
 
-            builder.Services.AddHttpClient();
+            builder.Services.ConfigureHttpClientDefaults(http =>
+            {
+                http.AddStandardResilienceHandler(o =>
+                {
+                    o.AttemptTimeout.Timeout = TimeSpan.FromSeconds(600);
+                    o.TotalRequestTimeout.Timeout = TimeSpan.FromSeconds(1200); // Must be larger than AttemptTimeout
+                    o.CircuitBreaker.SamplingDuration = TimeSpan.FromSeconds(1200); //Must be at least 2 x AttemptTimeout
+                });
+
+                // Turn on service discovery by default
+                //http.UseServiceDiscovery();
+            });
+            builder.Services.AddHttpClient("llm", c =>
+            {
+                c.BaseAddress = new Uri($"{builder.Configuration.GetSection("Gemini").GetSection("EndpointUrl").Value}");
+                c.Timeout = TimeSpan.FromSeconds(540);
+            });
+
             builder.Services.AddScoped<IGeminiChatService, GeminiChatService>();
             return builder;
         }
